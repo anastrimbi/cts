64d85373e29f3306d0dab1d6a26c4b10cba1c739
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-03-03 20:34:20 +0200

    added Persoana class

--- Seminar 1/src/ro/ase/csie/cts/seminar1/Test.java
+++ Seminar 1/src/ro/ase/csie/cts/seminar1/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		Persoana p1 = new Persoana("Ana", "Strimbeanu");
+		System.out.println(p1.toString());
 	}
 
 }
