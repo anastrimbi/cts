40867c2491aa1865a18520a3a3c5be05fa45f262
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-03-04 14:15:59 +0200

    implementing vector for weekdays

--- Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
@@ -3,11 +3,15 @@ package ro.ase.csie.cts.seminar2;
 public class Main {
 
 	public static void main(String[] args) {
+		// instance of calendar util
 		CalendarUtil cu= new CalendarUtil();
+		// else if
 		System.out.println(cu.weekDay(3));
-		
-		CalendarUtil cu2 = new CalendarUtil();
+		// switch
 		System.out.println(cu.weekDay2(3));
+		// vector
+		System.out.println(cu.weekDay3(3));
+		
 	}
 
 }
