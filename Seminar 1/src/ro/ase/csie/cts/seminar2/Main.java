8bca12fbe2d0c9ca51fa4713864febf9c469e946
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-03-04 14:09:49 +0200

    same function with switch instead of else if

--- Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		CalendarUtil cu= new CalendarUtil();
 		System.out.println(cu.weekDay(3));
+		
+		CalendarUtil cu2 = new CalendarUtil();
+		System.out.println(cu.weekDay2(3));
 	}
 
 }
