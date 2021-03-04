f1e9a3aba2b3aedeb9378fc9027fed14ee54a3a3
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-03-04 15:14:29 +0200

    implemented person, bank account, some code in Main

--- Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
+++ Seminar 1/src/ro/ase/csie/cts/seminar2/Main.java
@@ -1,31 +1,48 @@
 package ro.ase.csie.cts.seminar2;
 
+import java.math.BigDecimal;
+
+import ro.ase.csie.cts.seminar2.solid.BankAccount;
+import ro.ase.csie.cts.seminar2.solid.Person;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// instance of calendar util
-		CalendarUtil cu= new CalendarUtil();
+//		// instance of calendar util
+//		CalendarUtil cu= new CalendarUtil();
+//		
+//		
+//		// exception
+//		try {
+//			System.out.println(cu.weekDay(10));
+//			// else if
+//			System.out.println(cu.weekDay(3));
+//			// switch
+//			System.out.println(cu.weekDay2(3));
+//			// vector
+//			System.out.println(cu.weekDay3(3));
+//			// validation working?
+//			System.out.println(cu.weekDay3(10));
+//			// with pre-existing java class
+//			System.out.println("DateFormatSymbols says" + cu.weekDay4(10));
+//			System.out.println("DateFormatSymbols says" + cu.weekDay4(5));
+//		} catch (IncorrectDayException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
 		
+		Person p = new Person("Ana");
+		BankAccount account = new BankAccount("INGB123242342", p);
 		
-		// exception
-		try {
-			System.out.println(cu.weekDay(10));
-			// else if
-			System.out.println(cu.weekDay(3));
-			// switch
-			System.out.println(cu.weekDay2(3));
-			// vector
-			System.out.println(cu.weekDay3(3));
-			// validation working?
-			System.out.println(cu.weekDay3(10));
-			// with pre-existing java class
-			System.out.println("DateFormatSymbols says" + cu.weekDay4(10));
-			System.out.println("DateFormatSymbols says" + cu.weekDay4(5));
-		} catch (IncorrectDayException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		account.deposit(100);
+		account.withdraw(50);
+		
+		account.withdraw(70);
+		
+		System.out.println(account.getBalance());
+		
 	}
 
 }
