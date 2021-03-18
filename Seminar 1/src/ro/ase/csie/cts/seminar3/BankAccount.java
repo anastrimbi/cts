68c4de8d2c42e1b5aad042485157342ca11d1094
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-03-18 13:27:37 +0200

    bank account fixed

--- Seminar 1/src/ro/ase/csie/cts/seminar3/BankAccount.java
+++ Seminar 1/src/ro/ase/csie/cts/seminar3/BankAccount.java
@@ -1,33 +1,37 @@
 package ro.ase.csie.cts.seminar3;
 
 public abstract class BankAccount {
-    protected String iban;
-    protected long balance;
-    protected Person accountHolder;
-    
-    protected NotificationService notificationService;
+	protected String iban;
+	protected long balance;
+	protected Person accountHolder;
+	
+	protected NotificationService notificationService;
+	
+	public BankAccount(NotificationService notificationService) {
+		this.notificationService = notificationService;
+	}
 
-    public String getIban() {
-        return iban;
-    }
+	public String getIban() {
+		return iban;
+	}
 
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
 
-    public long getBalance() {
-        return balance;
-    }
+	public long getBalance() {
+		return balance;
+	}
 
-    public void setBalance(long balance) {
-        this.balance = balance;
-    }
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
 
-    public Person getAccountHolder() {
-        return accountHolder;
-    }
+	public Person getAccountHolder() {
+		return accountHolder;
+	}
 
-    public void setAccountHolder(Person accountHolder) {
-        this.accountHolder = accountHolder;
-    }
+	public void setAccountHolder(Person accountHolder) {
+		this.accountHolder = accountHolder;
+	}
 }
