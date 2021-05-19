69587f6f5ab99bdd63b7b0ceb22a8e36f2379635
Ana-Maria Strimbeanu <anastrimbi@gmail.com>
2021-05-19 14:58:06 +0300

    Implementare JUnit

---
+++ Laborator12 - Testare unitara/src/ro/ase/csie/cts/g1087/testare/modele/OperatiiMatematice.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.cts.g1087.testare.modele;
+
+import ro.ase.csie.cts.g1087.testare.exceptii.ExceptieImpartireLaZero;
+
+public class OperatiiMatematice {
+
+	// putem sa avem bug de overflow
+	public static int aduna(int a, int b) {
+		return a + b;
+	}
+	
+	public static double impartire(int a, int b) throws ExceptieImpartireLaZero {
+		if(b == 0)
+			throw new ExceptieImpartireLaZero();
+		return a/b;
+	}
+	
+	
+}
